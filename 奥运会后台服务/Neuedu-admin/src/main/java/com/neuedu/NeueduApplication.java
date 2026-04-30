97318b075d5e594a7e7612a2cd9ae3c2af5b2ab1
Jay Lee <myjay0906@gmail.com>
2026-04-30 16:18:00 +0800

    test

---
+++ 奥运会后台服务/Neuedu-admin/src/main/java/com/neuedu/NeueduApplication.java
@@ -0,0 +1,22 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author neuedu
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }, scanBasePackages = {"${ruoyi.auto-scan-package}"})
+public class NeueduApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(NeueduApplication.class, args);
+        System.out.println("N~N启动成功");
+    }
+}
+
